95f267e62d2c8445e30981ae37d2931b0dd64e0f
Peeyush Aggarwal <54816797+peeyush2007@users.noreply.github.com>
2021-08-26 01:39:40 +0530

    Add files via upload
    projects details

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World");
+            Console.ReadKey();
+
+        }
+    }
+
+}
